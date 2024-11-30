b50bf0ea3d753792fb0afe4d3627da9790b77ca5
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-12-01 00:12:08 +0100

    javadoc tag @author is pretty useless

--- src/main/java/change/me/please/package-info.java
+++ src/main/java/change/me/please/package-info.java
@@ -1,7 +1,13 @@
 /**
- * Classes implementationg
+ * Reference implementation of
  * {@link com.cthiebaud.passwordvalidator.PasswordValidator}
  * 
+ * <p>
+ * By <a href="https://github.com/cthiebaud">Christophe Thiebaud</a>
+ * </p>
+ * 
+ * <p>
  * CHANGE ME! PLEASE!
+ * </p>
  */
 package change.me.please;
